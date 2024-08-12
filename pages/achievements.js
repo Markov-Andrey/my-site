9805a8dddf0f38f98c261cc4cf2f75e4bfd4ac39
Andre Markov <andremarkov@icloud.com>
2024-08-12 17:19:28 +0300

    add achievements

--- pages/achievements.js
+++ pages/achievements.js
@@ -1,11 +1,15 @@
 function renderImageAchievement(achievement) {
     return `
         <div class="bg-white dark:bg-gray-900 shadow-lg rounded-lg overflow-hidden">
-            <img 
-                src="${achievement.image}" 
-                alt="${achievement.title}" 
-                class="w-full h-96 object-contain"
-            >
+            <div class="w-full h-96">
+                <img 
+                    src="${achievement.image}" 
+                    alt="${achievement.title}" 
+                    data-modal-toggle="imageModal"
+                    data-image-src="${achievement.image}"
+                    class="m-auto h-96 object-contain cursor-pointer"
+                >
+            </div>
             <div class="p-4">
                 <h2 class="text-xl font-semibold text-gray-900 dark:text-white mb-2">
                     ${achievement.title}
@@ -47,7 +51,7 @@ function renderVideoAchievement(achievement) {
                 <div class="aspect-w-16 aspect-h-9">
                     <iframe 
                         class="w-full h-96 rounded object-contain" 
-                        src="${achievement.link.replace('watch?v=', 'embed/')}" 
+                        src="${achievement.link}" 
                         title="${achievement.title}" 
                         allowfullscreen>
                     </iframe>
@@ -66,30 +70,30 @@ function renderVideoAchievement(achievement) {
 function achievementsPage() {
     const achievements = [
         {
-            title: "Белгосохота",
-            description: "Охота с компанией Белгосохота, популярное мероприятие в Беларуси.",
-            image: 'public/images/achievements/belgosohota.webp',
-            link: '#',
-            type: 'image'
-        },
-        {
-            title: "MyITSchool на YouTube",
-            description: "Видео на YouTube, где рассказывается о проекте MyITSchool.",
-            link: 'https://youtu.be/at-I2tnP_J8?si=0Cn8N7NgjczDq8Pg',
-            type: 'video'
-        },
-        {
-            title: "Onliner: Лучшие истории",
-            description: "Моя статья на Onliner о лучших историях о тратах на еду.",
+            title: "Onliner: Бонусная статья к конкурсу о тратах на еду",
+            description: "Внеконкурсная статья о BudgetOwl, карманном помощнике по учету семейного бюджета.",
             link: 'https://money.onliner.by/2024/08/11/traty-na-edu-luchshie-istorii',
             type: 'link'
         },
         {
             title: "CutCode: Moonshine",
-            description: "Статья на CutCode о проекте Moonshine и учете прибывающего транспорта.",
+            description: "Статья на CutCode об использовании админ-панели Moonshine для разработки сайта по менеджменту транспорта на территории склада логистической компании.",
             link: 'https://cutcode.dev/articles/moonshine-keis-1-ucet-pribyvaiushhego-transporta',
             type: 'link'
-        }
+        },
+        {
+            title: "Белгосохота",
+            description: "За успешную работу в компании и достижения. За профессионально изготавливаемую картографию.",
+            image: 'public/images/achievements/belgosohota.webp',
+            link: '#',
+            type: 'image'
+        },
+        {
+            title: "Mоя IT Школа. Интервью",
+            description: 'С учебной "скамьи" сразу в "прод". Самый успешный в учебной группе практически сразу начал строить карьеру в интересующей меня сфере',
+            link: 'https://www.youtube.com/embed/at-I2tnP_J8?si=tZsH3JqlM7xR522O',
+            type: 'video'
+        },
     ];
 
     return `
