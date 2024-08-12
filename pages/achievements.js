9261a7aea902af293cd89f90f8ddd0a4efce5120
Andre Markov <andremarkov@icloud.com>
2024-08-12 17:41:45 +0300

    add achievements

--- pages/achievements.js
+++ pages/achievements.js
@@ -94,6 +94,12 @@ function achievementsPage() {
             link: 'https://www.youtube.com/embed/at-I2tnP_J8?si=tZsH3JqlM7xR522O',
             type: 'video'
         },
+        {
+            title: "БГСХА. Студенческая наука",
+            description: 'Участие в республиканском конкурсе научных работ студентов. Присвоена 1-я категория за работу "Влияние ионизирующего излучения на лежкость сахарной свеклы"',
+            link: 'http://www.sws.bsu.by/%D0%90%D1%80%D1%85%D0%B8%D0%B2/%D0%90%D1%80%D1%85%D0%B8%D0%B22014/%D0%A0%D0%B5%D0%B7%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%82%D1%8B%20%D0%BF%D0%BE%20%D0%B2%D1%83%D0%B7%D0%B0%D0%BC-%D1%81%D0%B5%D0%BA%D1%86-%D0%BA%D0%B0%D1%82%D0%B5%D0%B3(%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D1%8B_%D1%80%D1%83%D0%BA%D0%BE%D0%B2_%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0)%D0%B4%D0%BB%D1%8F%20%D1%81%D0%B0%D0%B9%D1%82%D0%B0.pdf',
+            type: 'link'
+        },
     ];
 
     return `
